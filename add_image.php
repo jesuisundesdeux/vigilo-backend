4be87ca213cf01d2263a41149919fc74e4649643
Kentiss <hessquentin@gmail.com>
2019-01-16 22:52:20 +0100

    type fichier

--- add_image.php
+++ add_image.php
@@ -4,9 +4,9 @@ $token = $_GET['token'];
 $data = file_get_contents('php://input');
 $status = 0;
 
-# FIXME : securiser $token et type de fichier
-
-if (!(file_put_contents('images/'.$token.".jpg",$data) === FALSE)) ; 
+$filename = preg_replace('/[^A-Za-z0-9]/', '', $token);
+# FIXME : securiser type fichier
+if (!(file_put_contents('images/'.$filename.".jpg",$data) === FALSE)) ; 
 else $status = 1;
 
 echo json_encode(array('status'=>$status));
