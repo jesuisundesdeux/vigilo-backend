7d98fb2c17eee48dabdde4526689f20190b430b0
Kentiss <hessquentin@gmail.com>
2019-01-17 09:48:27 +0100

    add log and exception : create_issue

--- create_issue.php
+++ create_issue.php
@@ -1,5 +1,7 @@
 <?php
 require_once('./common.php');
+$status = 0;
+
 function RandomStringGenerator($n) 
 { 
     // Variable which store final string 
@@ -36,14 +38,27 @@ $coordinates_lat = mysqli_real_escape_string($db,$_POST['coordinates_lat']);
 $coordinates_lon = mysqli_real_escape_string($db,$_POST['coordinates_lon']);
 $comment = mysqli_real_escape_string($db,$_POST['comment']);
 $categorie = mysqli_real_escape_string($db,$_POST['categorie']);
+
 $json = array('token' => $token, 'status' => 0);
+error_log(!empty($coordinates_lat).'-'.!empty($coordinates_lon) .'-'.!empty($comment).'-'.!empty($categorie));
+if(!empty($coordinates_lat) and !empty($coordinates_lon) and !empty($comment) and !empty($categorie)) {
 
+  mysqli_query($db,'INSERT INTO obs_list (`obs_coordinates_lat`,`obs_coordinates_lon`,`obs_comment`,`obs_categorie`,`obs_token`,`obs_time`,`obs_status`) VALUES
+				  ("'.$coordinates_lat.'","'.$coordinates_lon.'","'.$comment.'","'.$categorie.'","'.$token.'","'.time().'",0)') ;
 
-mysqli_query($db,'INSERT INTO obs_list (`obs_coordinates_lat`,`obs_coordinates_lon`,`obs_comment`,`obs_categorie`,`obs_token`,`obs_time`,`obs_status`) VALUES
-                                  ("'.$coordinates_lat.'","'.$coordinates_lon.'","'.$comment.'","'.$categorie.'","'.$token.'","'.time().'",0)') ;
-if($mysqlerror = mysqli_error($db)) {
-	$json['status'] = 1;
-	error_log($mysqlerror);
+  if($mysqlerror = mysqli_error($db)) {
+    $status = 1;
+    error_log('CREATE_ISSUE : MySQL Error '.$mysqlerror);
+  }
+}
+else {
+  $status = 1;
+  error_log('CREATE_ISSUE : Field not supported');
+}
+
+$json['status'] = $status;
+if($status != 0) {
+	    http_response_code(500);
 }
 echo json_encode($json);
 ?>
