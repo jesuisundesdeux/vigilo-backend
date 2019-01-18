8700b62bad7165bfaf8f826cf092b470e73cb55d
Bruno Adelé <bruno@adele.im>
2019-01-18 08:30:58 +0100

    Fix database creation

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ docker-compose up
 
 *Test new creation*
 ```
-curl -d 'coordinates_lat=0&coordinates_lon=0&comment=comment&categorie=1' -X POST 'http://localhost/create_issue.php'
+curl -d 'coordinates_lat=43.6&coordinates_lon=3.9&comment=comment&categorie=1' -X POST 'http://localhost/create_issue.php'
 ```
 
 *phpmyadmin*
--- init.sql
+++ init.sql
@@ -2,17 +2,18 @@ CREATE DATABASE IF NOT EXISTS dbname;
 
 USE dbname;
 CREATE TABLE `obs_list` (
- `obs_id` int(11) NOT NULL,
+ `obs_id` int(11) AUTO_INCREMENT NOT NULL,
  `obs_coordinates_lat` varchar(255) COLLATE utf8_bin NOT NULL,
  `obs_coordinates_lon` varchar(255) COLLATE utf8_bin NOT NULL,
  `obs_comment` text COLLATE utf8_bin NOT NULL,
  `obs_categorie` smallint(6) NOT NULL DEFAULT 1,
  `obs_token` varchar(30) COLLATE utf8_bin NOT NULL,
  `obs_time` bigint(20) NOT NULL,
- `obs_status` smallint(6) NOT NULL
+ `obs_status` smallint(6) NOT NULL,
+ PRIMARY KEY (obs_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 ALTER TABLE `obs_list`
- ADD PRIMARY KEY (`obs_id`),
  ADD KEY `token` (`obs_token`);
 
  INSERT INTO `obs_list` (`obs_id`, `obs_coordinates_lat`, `obs_coordinates_lon`, `obs_comment`, `obs_categorie`, `obs_token`, `obs_time`, `obs_status`) VALUES
