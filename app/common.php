03f93702fb52bd61b18b199db0c2d5a34d6e5117
Kentiss <hessquentin@gmail.com>
2019-02-05 15:38:57 +0100

    add cat to common

--- app/common.php
+++ app/common.php
@@ -4,3 +4,17 @@ mysqli_set_charset($db, 'utf8' );
 
 # Generate Maps
 $mapquestapi_key=getenv("MAPQUEST_API");
+
+# Categories
+$categorie = array(
+1 => "Non défini",
+2 => "Objet ou vehicule bloquant le passage (GCUM, ...)",
+3 => "Aménagement incohérent",
+4 => "Entretien de voirie (nid de poule, ...)",
+5 => "Zone sans stationnement vélo",
+6 => "Coupure temporaire sans balisage (chantier, ...)",
+7 => "Incivilité récurrente sur la route (SAS velos encombré, ...)",
+8 => "Aménagement exemplaire",
+100 => "Autre");
+
+
