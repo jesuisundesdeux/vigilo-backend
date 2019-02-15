16252c41694158cffa09700f433f183735749487
Kentiss <hessquentin@gmail.com>
2019-02-15 14:00:47 +0100

    change nb obs in panel

--- app/generate_panel.php
+++ app/generate_panel.php
@@ -14,7 +14,6 @@ else {
   $key = Null;
 }
 
-
 $token = mysqli_real_escape_string($db, $_GET['token']);
 
 if(isset($_GET['secretid'])) {
@@ -56,16 +55,15 @@ if (mysqli_num_rows($query) == 1) {
     $approved = 1;
   }
 
-  #$nbsignalement = 1;
-  
   # Check closest issues
   $query_issues_coordinates = mysqli_query($db, "SELECT obs_coordinates_lat,obs_coordinates_lon,obs_time,obs_token FROM obs_list");
   $additionalmarkers = '';
+  $nbsimilarobs = 0;
   $color_recent = 'db0000';
   $color_month = 'db7800';
   $color_old = 'a8a8a8';
   while ($result_issues_coordinates = mysqli_fetch_array($query_issues_coordinates)) {
-    if (distance($coordinates_lat, $coordinates_lon, $result_issues_coordinates['obs_coordinates_lat'], $result_issues_coordinates['obs_coordinates_lon'], 'm') < 30 && $result_issues_coordinates['obs_token'] != $token) {
+    if (distance($coordinates_lat, $coordinates_lon, $result_issues_coordinates['obs_coordinates_lat'], $result_issues_coordinates['obs_coordinates_lon'], 'm') < 500 && $result_issues_coordinates['obs_token'] != $token) {
       $osb_time = $result_issues_coordinates['obs_time'];
       if (time() - $osb_time < 3600 * 24 * 30) {
         $color = $color_recent;
@@ -75,15 +73,15 @@ if (mysqli_num_rows($query) == 1) {
         $color = $color_old;
       }
 
- #     $nbsignalement++;
+      $nbsimilarobs++;
 
       $additionalmarkers .= $result_issues_coordinates['obs_coordinates_lat'] . ',' . $result_issues_coordinates['obs_coordinates_lon'] . '|via-md-' . $color . '||';
     }
   }
 
   
-  $nbsimilarobs_query = mysqli_query($db,'SELECT * FROM obs_list WHERE obs_group="'.$groupid.'"');
-  $nbsimilarobs = mysqli_num_rows($nbsimilarobs_query);
+#  $nbsimilarobs_query = mysqli_query($db,'SELECT * FROM obs_list WHERE obs_group="'.$groupid.'"');
+#  $nbsimilarobs = mysqli_num_rows($nbsimilarobs_query);
 
   ## Wide map
   $size = '390,350';
@@ -230,7 +228,7 @@ if (mysqli_num_rows($query) == 1) {
   imagecopymerge($image, $map_zoom, 5, 400, 0, 0, 390, 360, 100);
   
   # Nb Similar Obs
-  $tsimilarobs = $nbsimilarobs . ' observations pour ce problème';
+  $tsimilarobs = $nbsimilarobs . ' observations dans cette zone';
   imagefilledrectangle($image, 0, 730, 396, 760, $black);
   imagettftext($image, 14, 0, 10, 754, $white, $fontfile, $tsimilarobs);
 
