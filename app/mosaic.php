1a5b1266838d1330f72478d9af68ce5a0b51583f
Kentiss <hessquentin@gmail.com>
2019-02-20 10:45:50 +0100

    add group to mosaic

--- app/mosaic.php
+++ app/mosaic.php
@@ -11,6 +11,14 @@ if(isset($_GET['c'])) {
 else {
   $cat = 'all';
 }
+
+if(isset($_GET['g'])) {
+  $grp = $_GET['g'] ;
+}
+else {
+  $grp = 'all';
+}
+
 $url = 'https://vigilo.jesuisundesdeux.org/get_issues.php';
 
 $data = file_get_contents($url);
@@ -18,7 +26,7 @@ $data = file_get_contents($url);
 $content = json_decode($data,true);
 $item= 0;
 foreach($content as $value) {
-if($value['categorie'] == $cat OR $cat == 'all') {
+if(($value['categorie'] == $cat OR $cat == 'all') AND ($value['group'] == $grp OR $grp == 'all')) {
   $item++;
   echo '<a target="_blank" href="https://umap.openstreetmap.fr/en/map/vigilo_286846#19/'.$value['coordinates_lat'].'/'.$value['coordinates_lon'].'"><img width=20%" src="https://vigilo.jesuisundesdeux.org/generate_panel.php?token='.$value['token'].'&s=400" /></a>';
   if($item == 5) {
