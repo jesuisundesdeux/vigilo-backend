c20772532ec9d02fe360cf7dcb1f1dbe7a446b32
Yoann Sculo <yoann@printk.fr>
2019-03-30 20:14:32 +0100

    approve: exit sooner with 500 error code
    avoid any unnecessary indent

--- app/approve.php
+++ app/approve.php
@@ -15,32 +15,33 @@ else {
 $status = 0;
 $token = mysqli_real_escape_string($db, $token);
 
-if (getrole($key, $acls) == "admin") {
-  $checktoken_query = mysqli_query($db,"SELECT obs_token,obs_comment,obs_time,obs_coordinates_lat,obs_coordinates_lon FROM obs_list WHERE obs_token='".$token."' LIMIT 1");
-
-  if (mysqli_num_rows($checktoken_query) == 1) {
-
-    $query = mysqli_query($db, "UPDATE obs_list set obs_approved=1 WHERE obs_token='" . $token . "'");
-    delete_token_cache($token);
-
-    $checktoken_result = mysqli_fetch_array($checktoken_query);
-    $comment = $checktoken_result['obs_comment'];
-    $time = $checktoken_result['obs_time'];
-    $coordinates_lat = $checktoken_result['obs_coordinates_lat'];
-    $coordinates_lon = $checktoken_result['obs_coordinates_lon'];
-    if ($time > (time() - 3600 * 24)) {
-      $tweet_content = str_replace('[COMMENT]',$comment,$tweet_content);
-      $tweet_content = str_replace('[TOKEN]',$token,$tweet_content);
-      $tweet_content = str_replace('[COORDINATES_LON]',$coordinates_lon,$tweet_content);
-      $tweet_content = str_replace('[COORDINATES_LAT]',$coordinates_lat,$tweet_content);
-      tweet($tweet_content, 'https://'.$_SERVER['SERVER_NAME'].'/generate_panel.php?token='.$token,$twitter_ids);
-    }
-  }
+/* Only admin can approve an observation */
+if (getrole($key, $acls) != "admin") {
+  error_log("ADD_IMAGE : Token : ".$token." and/or key not valid.");
+  http_response_code(500);
+  echo json_encode(array('status'=>1));
+  return;
 }
-else {
-    error_log("ADD_IMAGE : Token : ".$token." and/or key not valid.");
-    $status = 1;
 
+$checktoken_query = mysqli_query($db,"SELECT obs_token,obs_comment,obs_time,obs_coordinates_lat,obs_coordinates_lon FROM obs_list WHERE obs_token='".$token."' LIMIT 1");
+
+if (mysqli_num_rows($checktoken_query) == 1) {
+
+  $query = mysqli_query($db, "UPDATE obs_list set obs_approved=1 WHERE obs_token='" . $token . "'");
+  delete_token_cache($token);
+
+  $checktoken_result = mysqli_fetch_array($checktoken_query);
+  $comment = $checktoken_result['obs_comment'];
+  $time = $checktoken_result['obs_time'];
+  $coordinates_lat = $checktoken_result['obs_coordinates_lat'];
+  $coordinates_lon = $checktoken_result['obs_coordinates_lon'];
+  if ($time > (time() - 3600 * 24)) {
+    $tweet_content = str_replace('[COMMENT]',$comment,$tweet_content);
+    $tweet_content = str_replace('[TOKEN]',$token,$tweet_content);
+    $tweet_content = str_replace('[COORDINATES_LON]',$coordinates_lon,$tweet_content);
+    $tweet_content = str_replace('[COORDINATES_LAT]',$coordinates_lat,$tweet_content);
+    tweet($tweet_content, 'https://'.$_SERVER['SERVER_NAME'].'/generate_panel.php?token='.$token,$twitter_ids);
+  }
 }
 
 if ($status != 0) {
