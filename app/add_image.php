f96ac5b76b64db267bd3f0784a5c46a1aafa885f
Yoann Sculo <yoann@printk.fr>
2019-03-30 14:16:05 +0100

    add_image: abort if token or secretid are missing
    Don't do any useless mysql query if one of those parameters are
    missing. Since it will eventually lead to an error.

--- app/add_image.php
+++ app/add_image.php
@@ -7,8 +7,17 @@ $token = $_GET['token'];
 $secretid = $_GET['secretid'];
 
 $status = 0;
-$token = mysqli_real_escape_string($db, $token);
-$secretid = mysqli_real_escape_string($db, $secretid);
+
+if (isset($_GET['token']) && isset($_GET['secretid'])) {
+  $token = mysqli_real_escape_string($db, $token);
+  $secretid = mysqli_real_escape_string($db, $secretid);
+} else {
+  error_log("ADD_IMAGE : Missing token and/or secretid parameters.");
+  http_response_code(500);
+  echo json_encode(array('status'=>1));
+  return;
+}
+
 $checktoken_query = mysqli_query($db,"SELECT obs_token FROM obs_list WHERE obs_token='".$token."' AND obs_secretid='".$secretid."' LIMIT 1");
 
 if(mysqli_num_rows($checktoken_query) == 1) {
