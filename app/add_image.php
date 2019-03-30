534ae915998827b5f6fb49f7462973d6ea43bb74
Yoann Sculo <yoann@printk.fr>
2019-03-30 14:26:34 +0100

    add_image: abort sooner if token and secretid are not found
    avoid unnecessary indent and increase readability

--- app/add_image.php
+++ app/add_image.php
@@ -20,34 +20,35 @@ if (isset($_GET['token']) && isset($_GET['secretid'])) {
 
 $checktoken_query = mysqli_query($db,"SELECT obs_token FROM obs_list WHERE obs_token='".$token."' AND obs_secretid='".$secretid."' LIMIT 1");
 
-if(mysqli_num_rows($checktoken_query) == 1) {
-  $data = file_get_contents('php://input');
+if (mysqli_num_rows($checktoken_query) != 1) {
+  error_log("ADD_IMAGE : Token : ".$token." and/or secretid : ".$secretid." do not exist.");
+  http_response_code(500);
+  echo json_encode(array('status'=>1));
+  return;
+}
 
-  $filename = preg_replace('/[^A-Za-z0-9]/', '', $token);
+$data = file_get_contents('php://input');
 
-  if (!(file_put_contents('images/'.$filename.'.jpg',$data))) {
-    error_log('ADD_IMAGE : Error uploading image');
+$filename = preg_replace('/[^A-Za-z0-9]/', '', $token);
+
+if (!(file_put_contents('images/'.$filename.'.jpg',$data))) {
+  error_log('ADD_IMAGE : Error uploading image');
+  $status = 1;
+}
+else {
+  $allowedTypes = array(IMAGETYPE_JPEG);
+  $detectedType = exif_imagetype('images/'.$filename.'.jpg');
+  if(!array($detectedType, $allowedTypes)) {
+    unlink('images/'.$filename.'.jpg');
+    error_log('ADD_IMAGE : File type not supported : '. $detectedType);
     $status = 1;
   }
   else {
-    $allowedTypes = array(IMAGETYPE_JPEG);
-    $detectedType = exif_imagetype('images/'.$filename.'.jpg');
-      if(!array($detectedType, $allowedTypes)) {
-        unlink('images/'.$filename.'.jpg');
-        error_log('ADD_IMAGE : File type not supported : '. $detectedType);
-        $status = 1;
-      }
-      else {
-        mysqli_query($db,"UPDATE obs_list SET obs_complete=1 WHERE obs_token='".$token."' AND obs_secretid='".$secretid."'");
-      }
-   }
+    mysqli_query($db,"UPDATE obs_list SET obs_complete=1 WHERE obs_token='".$token."' AND obs_secretid='".$secretid."'");
+  }
 }
-else {
-    error_log("ADD_IMAGE : Token : ".$token." and/or secretid : ".$secretid." do not exist.");
-    $status = 1;
 
-}
-if($status != 0) {
+if ($status != 0) {
     http_response_code(500);
 }
 echo json_encode(array('status'=>$status));
