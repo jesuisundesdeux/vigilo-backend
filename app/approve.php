6b66f00d816d7284b88bd7ea76fda138cfdfe46f
Yoann Sculo <yoann@printk.fr>
2019-03-30 20:34:06 +0100

    approve: abort sooner if token is not found
    avoid unnecessary indent and increase readability

--- app/approve.php
+++ app/approve.php
@@ -23,25 +23,32 @@ if (getrole($key, $acls) != "admin") {
   return;
 }
 
+/* Check existence of token database */
 $checktoken_query = mysqli_query($db,"SELECT obs_token,obs_comment,obs_time,obs_coordinates_lat,obs_coordinates_lon FROM obs_list WHERE obs_token='".$token."' LIMIT 1");
+if (mysqli_num_rows($checktoken_query) != 1) {
+  error_log("APPROVE : Token : ".$token." does not exist.");
+  http_response_code(500);
+  echo json_encode(array('status'=>1));
+  return;
+}
 
-if (mysqli_num_rows($checktoken_query) == 1) {
+/* Set the observation to approved */
+$query = mysqli_query($db, "UPDATE obs_list set obs_approved=1 WHERE obs_token='" . $token . "'");
 
-  $query = mysqli_query($db, "UPDATE obs_list set obs_approved=1 WHERE obs_token='" . $token . "'");
-  delete_token_cache($token);
+/* Now remove the cache for this observation to remove blurring */
+delete_token_cache($token);
 
-  $checktoken_result = mysqli_fetch_array($checktoken_query);
-  $comment = $checktoken_result['obs_comment'];
-  $time = $checktoken_result['obs_time'];
-  $coordinates_lat = $checktoken_result['obs_coordinates_lat'];
-  $coordinates_lon = $checktoken_result['obs_coordinates_lon'];
-  if ($time > (time() - 3600 * 24)) {
-    $tweet_content = str_replace('[COMMENT]',$comment,$tweet_content);
-    $tweet_content = str_replace('[TOKEN]',$token,$tweet_content);
-    $tweet_content = str_replace('[COORDINATES_LON]',$coordinates_lon,$tweet_content);
-    $tweet_content = str_replace('[COORDINATES_LAT]',$coordinates_lat,$tweet_content);
-    tweet($tweet_content, 'https://'.$_SERVER['SERVER_NAME'].'/generate_panel.php?token='.$token,$twitter_ids);
-  }
+$checktoken_result = mysqli_fetch_array($checktoken_query);
+$comment = $checktoken_result['obs_comment'];
+$time = $checktoken_result['obs_time'];
+$coordinates_lat = $checktoken_result['obs_coordinates_lat'];
+$coordinates_lon = $checktoken_result['obs_coordinates_lon'];
+if ($time > (time() - 3600 * 24)) {
+  $tweet_content = str_replace('[COMMENT]',$comment,$tweet_content);
+  $tweet_content = str_replace('[TOKEN]',$token,$tweet_content);
+  $tweet_content = str_replace('[COORDINATES_LON]',$coordinates_lon,$tweet_content);
+  $tweet_content = str_replace('[COORDINATES_LAT]',$coordinates_lat,$tweet_content);
+  tweet($tweet_content, 'https://'.$_SERVER['SERVER_NAME'].'/generate_panel.php?token='.$token,$twitter_ids);
 }
 
 if ($status != 0) {
