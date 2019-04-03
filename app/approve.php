e58ef90849fb4216ababc63ee2a7a53750697c0a
Kentiss <hessquentin@gmail.com>
2019-04-03 11:21:08 +0200

    add approve filter on approveh.php

--- app/approve.php
+++ app/approve.php
@@ -41,6 +41,11 @@ if (getrole($key, $acls) != "admin") {
   return;
 }
 
+$approved = 1;
+if (isset($_GET['approved']) and is_numeric($_GET['approved'])) {
+  $approved = intval($_GET['approved']);
+}
+
 /* Check existence of token database */
 $checktoken_query = mysqli_query($db,"SELECT obs_token,obs_comment,obs_time,obs_coordinates_lat,obs_coordinates_lon FROM obs_list WHERE obs_token='".$token."' LIMIT 1");
 if (mysqli_num_rows($checktoken_query) != 1) {
@@ -51,29 +56,30 @@ if (mysqli_num_rows($checktoken_query) != 1) {
 }
 
 /* Set the observation to approved */
-$query = mysqli_query($db, "UPDATE obs_list set obs_approved=1 WHERE obs_token='" . $token . "'");
+$query = mysqli_query($db, "UPDATE obs_list set obs_approved=".$approved." WHERE obs_token='" . $token . "'");
 
-/* Now remove the cache for this observation to remove blurring */
-delete_token_cache($token);
-
-$checktoken_result = mysqli_fetch_array($checktoken_query);
-$comment = $checktoken_result['obs_comment'];
-$time = $checktoken_result['obs_time'];
-$coordinates_lat = $checktoken_result['obs_coordinates_lat'];
-$coordinates_lon = $checktoken_result['obs_coordinates_lon'];
-
-/* Don't tweet observations if they are more than N-hours old */
-$expiry_time = getenv("TWITTER_EXPIRY_TIME");
-if ($time > (time() - 3600 * $expiry_time)) {
-  $tweet_content = str_replace('[COMMENT]', $comment, $tweet_content);
-  $tweet_content = str_replace('[TOKEN]', $token, $tweet_content);
-  $tweet_content = str_replace('[COORDINATES_LON]', $coordinates_lon, $tweet_content);
-  $tweet_content = str_replace('[COORDINATES_LAT]',$coordinates_lat, $tweet_content);
-  tweet($tweet_content, 'https://'.$_SERVER['SERVER_NAME'].'/generate_panel.php?token='.$token, $twitter_ids);
-} else {
-  error_log("APPROVE : Token : ".$token." older than ".$expiry_time."h. We won't tweet it.");
+if($approved == 1) {
+  /* Now remove the cache for this observation to remove blurring */
+  delete_token_cache($token);
+  
+  $checktoken_result = mysqli_fetch_array($checktoken_query);
+  $comment = $checktoken_result['obs_comment'];
+  $time = $checktoken_result['obs_time'];
+  $coordinates_lat = $checktoken_result['obs_coordinates_lat'];
+  $coordinates_lon = $checktoken_result['obs_coordinates_lon'];
+  
+  /* Don't tweet observations if they are more than N-hours old */
+  $expiry_time = getenv("TWITTER_EXPIRY_TIME");
+  if ($time > (time() - 3600 * $expiry_time)) {
+    $tweet_content = str_replace('[COMMENT]', $comment, $tweet_content);
+    $tweet_content = str_replace('[TOKEN]', $token, $tweet_content);
+    $tweet_content = str_replace('[COORDINATES_LON]', $coordinates_lon, $tweet_content);
+    $tweet_content = str_replace('[COORDINATES_LAT]',$coordinates_lat, $tweet_content);
+    tweet($tweet_content, 'https://'.$_SERVER['SERVER_NAME'].'/generate_panel.php?token='.$token, $twitter_ids);
+  } else {
+    error_log("APPROVE : Token : ".$token." older than ".$expiry_time."h. We won't tweet it.");
+  }
 }
-
 if ($status != 0) {
     http_response_code(500);
 }
