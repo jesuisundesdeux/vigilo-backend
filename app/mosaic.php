b3c4cf23cc5270c7d4a3aa31d129639d092b3459
Yoann Sculo <yoann@printk.fr>
2019-04-07 17:29:07 +0200

    mosaic: html code cleanup

--- app/mosaic.php
+++ app/mosaic.php
@@ -23,29 +23,26 @@ require_once('./functions.php');
 <!-- https://codepen.io/desandro/full/RPKgEN -->
 <html>
 <head>
- <title>JeSuisUnDesDeux / Vigilo</title>
- <meta charset="UTF-8">
- <link href="/style/mosaic.css" type="text/css" rel="stylesheet">
+  <title>JeSuisUnDesDeux / Vigilo</title>
+  <meta charset="UTF-8">
+  <link href="/style/mosaic.css" type="text/css" rel="stylesheet">
 
   <script>
   window.console = window.console || function(t) {};
-</script>
-
-
+  </script>
 
   <script>
   if (document.location.search.match(/type=embed/gi)) {
     window.parent.postMessage("resize", "*");
   }
-</script>
-
+  </script>
 </head>
+
 <body bgcolor="#000000">
-<div class="grid">
-<div class="grid-sizer"></div>
+  <div class="grid">
+  <div class="grid-sizer"></div>
 
 <?php
-
 
 if (isset($_GET['c']) AND !empty($_GET['c'])) {
   $cat = $_GET['c'] ;
@@ -78,35 +75,28 @@ foreach ($content as $value) {
 }
 ?>
 </div>
-<script src="https://static.codepen.io/assets/common/stopExecutionOnTimeout-de7e2ef6bfefd24b79a3f68b414b87b8db5b08439cac3f1012092b2290c719cd.js"></script>
+  <script src="https://static.codepen.io/assets/common/stopExecutionOnTimeout-de7e2ef6bfefd24b79a3f68b414b87b8db5b08439cac3f1012092b2290c719cd.js"></script>
 
-<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.js'></script>
-<script src='https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.js'></script>
+  <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.js'></script>
+  <script src='https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.js'></script>
 
+  <script >
+    // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
-
-    <script >
-      // external js: masonry.pkgd.js, imagesloaded.pkgd.js
-
-// init Masonry
-var $grid = $('.grid').masonry({
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  columnWidth: '.grid-sizer'
-});
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry();
-});
-      //# sourceURL=pen.js
-    </script>
-
-
-
-
-
+    // init Masonry
+    var $grid = $('.grid').masonry({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      columnWidth: '.grid-sizer'
+    });
+    // layout Masonry after each image loads
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry();
+    });
+    //# sourceURL=pen.js
+  </script>
 
   <script src="https://static.codepen.io/assets/editor/live/css_reload-5619dc0905a68b2e6298901de54f73cefe4e079f65a75406858d92924b4938bf.js"></script>
-</body></html>
-
+</body>
+</html>
