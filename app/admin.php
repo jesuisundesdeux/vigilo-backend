b45335f5acaa8e9761e9fde788ada9f592376be9
Kentiss <hessquentin@gmail.com>
2019-04-17 15:53:54 +0200

    move admin

--- app/admin.php
+++ app/admin.php
@@ -17,65 +17,107 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#header('Cache-Control: no-cache, must-revalidate, max-age=0');
+
 require_once('./common.php');
 require_once('./functions.php');
 
-$realm = 'Accès restreint';
+$realm = 'Acces restreint';
 
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    header('WWW-Authenticate: Basic realm="'.$realm.'"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo 'Accès interdit';
-    http_response_code(403);
-    exit;
+  header('HTTP/1.1 401 Authorization Required');
+  header('WWW-Authenticate: Basic realm="Access denied"');
+  echo 'Acces interdit';
+  exit;
 }
 
 $http_login = $_SERVER['PHP_AUTH_USER'];
 $http_password = $_SERVER['PHP_AUTH_PW'];
 
-$query_roles = mysqli_query($db, "SELECT role_login,role_password FROM obs_roles WHERE role_login='".$role_login."' LIMIT 1");
+$query_roles = mysqli_query($db, "SELECT role_key,role_login,role_password FROM obs_roles WHERE role_login='".$http_login."' LIMIT 1");
 if(mysqli_num_rows($query_roles) != 1) {
-  http_response_code(403);
-  die('Accès interdit');
+  header('HTTP/1.1 401 Authorization Required');
+  header('WWW-Authenticate: Basic realm="Access denied"');
+  echo 'Acces interdit';
+  exit;
+
 }
 $result_role = mysqli_fetch_array($query_roles);
 
 if(hash('sha256',$http_password) != $result_role['role_password']) {
-    http_response_code(403);
-    die('Accès interdit');
+  header('HTTP/1.1 401 Authorization Required');
+  header('WWW-Authenticate: Basic realm="Access denied"');
+  echo 'Acces interdit';
+  exit;
 }
 
-$sresample=200;
+$role_key = $result_role['role_key'];
 
-# Get issue information
-$query = mysqli_query($db, "SELECT * FROM obs_list ORDER BY obs_time DESC");
-
-if (mysqli_num_rows($query) > 0) {
-  echo '<meta http-equiv="content-type" content="text/html; charset=utf-8" />';
-  echo '<table cellpadding="0" cellspacing="0" class="db-table">';
-  echo '<tr><th>time</th><th>street_name</th><th>comment</th><th>categorie<th>photo</th><th>approved</th><th>delete</th></tr>';
-  while ($result = mysqli_fetch_array($query)) {
-    $coordinates_lat = $result['obs_coordinates_lat'];
-    $coordinates_lon = $result['obs_coordinates_lon'];
-    $street_name = $result['obs_address_string'];
-    $comment = $result['obs_comment'];
-    $categorie = $result['obs_categorie'];
-    $token = $result['obs_token'];
-    $time = $result['obs_time'];
-    $status = $result['obs_status'];
-    $version = $result['obs_app_version'];
-    $approved = $result['obs_approved'];
-    $secretid = $result['obs_secretid'];
-
-    echo '<tr>';
-    echo '<td>'.$time.'</td>';
-    echo '<td>'.$street_name.'</td>';
-    echo '<td>'.$comment.'</td>';
-    echo '<td>'.$categorie.'</td>';
-    echo '<td><a href="/admin/photo.php?token='.$token.'" target="_blank"><img src="/generate_panel.php?s='.$sresample.'&token='.$token.'"</img><a/></td>';
-    echo '<td>'.$approved.'</td>';
-    echo '<td><a href="/delete.php?token='.$token.'&secretid='.$secretid.'">Supprimer</a></td>';
-    echo "</tr>\n";
+if(isset($_GET['action']) || !empty($_GET['action'])) {
+  if($_GET['action'] == "photo") {
+    $token = mysqli_real_escape_string($db, $_GET['token']);
+    $secretid = mysqli_real_escape_string($db, $_GET['secretid']);
+    
+    # Get issue information
+    $query = mysqli_query($db, "SELECT * FROM obs_list WHERE obs_token = '$token' LIMIT 1");
+    
+    if (mysqli_num_rows($query) == 1) {
+        $result = mysqli_fetch_array($query);
+        $approved = $result['obs_approved'];
+      
+      
+      ## Init other images components :
+      echo '<meta http-equiv="content-type" content="text/html; charset=utf-8" />';
+      echo '<table cellpadding="0" cellspacing="0" class="db-table">';
+      echo '<tr><th>photo</th><th>action</th></tr>';
+    
+      echo '<tr>';
+      echo '<td><img src="/generate_panel.php?token='.$token.'&secretid='.$secretid.'"></img></td>';
+      echo '<td>';
+      echo '<a href="/approve.php?token='.$token.'&approved=1&key='.$role_key.'">Approuver photo</a><br />';
+      echo '<a href="/approve.php?token='.$token.'&approved=2&key='.$role_key.'">Desapprouver photo</a><br />';
+      echo 'STATUS:'.$approved;
+    
+      echo '</td>';
+      echo '</tr>';
+      echo '</table>';
+    }
   }
-  echo '</table><br />';
 }
+else {
+  $sresample=200;
+
+  # Get issue information
+  $query = mysqli_query($db, "SELECT * FROM obs_list ORDER BY obs_time DESC");
+
+  if (mysqli_num_rows($query) > 0) {
+    echo '<meta http-equiv="content-type" content="text/html; charset=utf-8" />';
+    echo '<table cellpadding="0" cellspacing="0" class="db-table">';
+    echo '<tr><th>time</th><th>street_name</th><th>comment</th><th>categorie<th>photo</th><th>approved</th><th>delete</th></tr>';
+    while ($result = mysqli_fetch_array($query)) {
+      $coordinates_lat = $result['obs_coordinates_lat'];
+      $coordinates_lon = $result['obs_coordinates_lon'];
+      $street_name = $result['obs_address_string'];
+      $comment = $result['obs_comment'];
+      $categorie = $result['obs_categorie'];
+      $token = $result['obs_token'];
+      $time = $result['obs_time'];
+      $status = $result['obs_status'];
+      $version = $result['obs_app_version'];
+      $approved = $result['obs_approved'];
+      $secretid = $result['obs_secretid'];
+  
+      echo '<tr>';
+      echo '<td>'.$time.'</td>';
+      echo '<td>'.$street_name.'</td>';
+      echo '<td>'.$comment.'</td>';
+      echo '<td>'.$categorie.'</td>';
+      echo '<td><a href="/admin.php?action=photo&token='.$token.'&secretid='.$secretid.'" target="_blank"><img src="/generate_panel.php?s='.$sresample.'&token='.$token.'"</img><a/></td>';
+      echo '<td>'.$approved.'</td>';
+      echo '<td><a href="/delete.php?token='.$token.'&secretid='.$secretid.'">Supprimer</a></td>';
+      echo "</tr>\n";
+    }
+    echo '</table><br />';
+  }
+}
+
