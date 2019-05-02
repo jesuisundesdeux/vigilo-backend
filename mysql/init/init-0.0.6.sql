f9606c84c35b0a9b4764ae24a09e071f14c018c6
Kentiss <hessquentin@gmail.com>
2019-05-02 11:38:35 +0200

    fix mysql version

--- mysql/init/init-0.0.6.sql
+++ mysql/init/init-0.0.6.sql
@@ -1,3 +1,1 @@
-ALTER TABLE `obs_list` CHANGE `obs_app_version` `obs_app_version` VARCHAR(50) NOT NULL;
-ALTER TABLE `obs_config` ADD PRIMARY KEY( `config_id`);
-UPDATE `obs_config` SET `config_value` = '0.0.5' WHERE `obs_config`.`config_param` = 'vigilo_db_version';
+UPDATE `obs_config` SET `config_value` = '0.0.6' WHERE `obs_config`.`config_param` = 'vigilo_db_version';
