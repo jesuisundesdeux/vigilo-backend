69ff1696ecffb74ef7e444a5fe69e638d2969d02
Yoann Sculo <yoann@printk.fr>
2019-05-11 13:25:19 +0200

    bump vigilo version to 0.0.8

--- mysql/init/init-0.0.8.sql
+++ mysql/init/init-0.0.8.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `obs_scopes` ADD `scope_twitter` VARCHAR(20) NOT NULL AFTER `scope_sharing_content_text`;
 ALTER TABLE `obs_scopes` ADD `scope_department` TINYINT NOT NULL AFTER `scope_display_name`;
+
+UPDATE `obs_config` SET `config_value` = '0.0.8' WHERE `obs_config`.`config_param` = 'vigilo_db_version';
