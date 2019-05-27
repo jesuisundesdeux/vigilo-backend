deeef63339102e2cb429d352c5d01e3e3861b05c
Yoann Sculo <yoannsculo@users.noreply.github.com>
2019-05-27 09:37:30 +0200

    upgrade: improve upgrade system and provide more explanation (#60)

--- doc/UPGRADE.md
+++ doc/UPGRADE.md
@@ -1,21 +1,28 @@
 ### Upgrade
 
-#### 0.0.7 to 0.0.8
-
-* Launch mysql/init/init-0.0.8.sql
-* Update sources code 
-
 #### 0.0.8 to 0.0.9
 
 ##### All
 
-* Launch mysql/init/init-0.0.9.sql in MySQL
+* Do NOT update your source code à this stage. Stay in 0.0.8.
+* Download from Github and launch mysql/init/init-0.0.9.sql in MySQL
+
+wget https://raw.githubusercontent.com/jesuisundesdeux/vigilo-backend/master/install_app/upgrade.php -O app/upgrade.php
+
 * Upload install_app/upgrade.php in the app root
+* Execute /upgrade.php and make sure there is no error
 * Execute /upgrade.php?from=0.0.8&to=0.0.9
-* Remove /upgrade.php
+* Clean config/config.php as explained in upgrade.php. (Remove all variables except for the « Database configuration » block with the following variables: MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_CHARSET)
+* Remove /upgrade.php from your app directory
 * Update sources code
-* Clean config.php (remove all ariables except in the bloc /* Database configuration */)
 
 ##### Docker only
 
+* Update your .env file and align it with .env_sample
 * Update docker_compose as docker_compose_sample
+
+#### 0.0.7 to 0.0.8
+
+* Launch mysql/init/init-0.0.8.sql
+* Update sources code
+
--- install_app/upgrade.php
+++ install_app/upgrade.php
@@ -2,10 +2,11 @@
 
 echo "<p>Ce fichier doit être mis à la racine de app/ et supprimé une fois executé</p>";
 
-require_once("includes/common.php");
+$cwd = dirname(__FILE__);
 
+require_once("${cwd}/includes/common.php");
 
-$query_version = mysqli_query($db,"SELECT * FROM obs_config WHERE config_param='vigilo_db_version'");
+$query_version = mysqli_query($db, "SELECT * FROM obs_config WHERE config_param='vigilo_db_version'");
 $query_result = mysqli_fetch_array($query_version);
 
 echo "<p>Version actuelle code: " . BACKEND_VERSION . '</p>';
@@ -15,29 +16,32 @@ echo "<p>Version de la base: " . $query_result['config_value'] . '</p>';
   echo "La base n'est pas à jour, veuillez executer dans l'ordre les fichiers de mysql/init/ à partir de init-" . $query_result['config_value'] . ".sql (exlue) jusqu'à init-".BACKEND_VERSION.".sql (inclus)" ; 
 }
 else {*/
-  if(isset($_GET['from']) && isset($_GET['to'])) {  
-	$from = $_GET['from']; 
-	$to = $_GET['to']; 
-	if($from == "0.0.8" && $to == "0.0.9") {
-	  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_urlbase','".$config['URLBASE']."')");
-	  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_http_proto','".$config['HTTP_PROTOCOL']."')");
-	  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_name','".$config['VIGILO_NAME']."')");
-	  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_language','".$config['VIGILO_LANGUAGE']."')");
-	  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_mapquest_api','".$config['MAPQUEST_API']."')");
-	  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('twitter_expiry_time','".$config['APPROVE_TWITTER_EXPTIME']."')");
-	  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('mysql_charset','".$config['MYSQL_CHARSET']."')");
-	  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_timezone','".$config['VIGILO_TIMEZONE']."')");
 
-	  mysqli_query($db, "INSERT INTO obs_twitteraccounts (`ta_consumer`,`ta_consumersecret`,`ta_accesstoken`,`ta_accesstokensecret`) 
-									VALUES ('".$config['TWITTER_IDS']['consumer']."',
-											'".$config['TWITTER_IDS']['consumersecret']."',
-											'".$config['TWITTER_IDS']['accesstoken']."',
-											'".$config['TWITTER_IDS']['accesstokensecret']."')");
+if (!isset($_GET['from']) || !isset($_GET['to'])) {
+  exit(1);
+}
+
+$from = $_GET['from'];
+$to = $_GET['to'];
+if ($from == "0.0.8" && $to == "0.0.9") {
+  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_urlbase','".$config['URLBASE']."')");
+  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_http_proto','".$config['HTTP_PROTOCOL']."')");
+  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_name','".$config['VIGILO_NAME']."')");
+  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_language','".$config['VIGILO_LANGUAGE']."')");
+  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_mapquest_api','".$config['MAPQUEST_API']."')");
+  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('twitter_expiry_time','".$config['APPROVE_TWITTER_EXPTIME']."')");
+  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('mysql_charset','".$config['MYSQL_CHARSET']."')");
+  mysqli_query($db, "INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_timezone','".$config['VIGILO_TIMEZONE']."')");
+
+  mysqli_query($db, "INSERT INTO obs_twitteraccounts (`ta_consumer`,`ta_consumersecret`,`ta_accesstoken`,`ta_accesstokensecret`)
+								VALUES ('".$config['TWITTER_IDS']['consumer']."',
+										'".$config['TWITTER_IDS']['consumersecret']."',
+										'".$config['TWITTER_IDS']['accesstoken']."',
+										'".$config['TWITTER_IDS']['accesstokensecret']."')");
 
 
-	  mysqli_query($db, "UPDATE obs_scopes SET scope_twittercontent = '".$config['TWITTER_CONTENT']."'"); 
-	  echo "<p>Le fichier config.php peut être nettoyé, supprimer tout sauf le bloc /* Database configuration */</p>";
-	}
-  }  
+  mysqli_query($db, "UPDATE obs_scopes SET scope_twittercontent = '".$config['TWITTER_CONTENT']."'");
+  echo "<ul><li>Le fichier config.php peut être nettoyé, supprimez tout son contenu sauf le bloc /* Database configuration */ (MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_CHARSET)</li><li>Supprimez le fichier upgrade.php du dossier app</li></ul>";
+}
 //}
 
