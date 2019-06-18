d58efa70032343ef55eeba4cdb2a54512fc8f4e8
kentiss34 <webmaster@my-heb.eu>
2019-06-18 15:57:26 +0200

    typo fix

--- app/includes/functions.php
+++ app/includes/functions.php
@@ -167,7 +167,7 @@ function jsonError($prefix, $error_msg, $internal_code="Unknown", $http_status_c
     $json = array("error" => array('status' => $http_status_code, "code" => $internal_code, "message" => $error_msg));
     http_response_code($http_status_code);
     echo json_encode($json, JSON_PRETTY_PRINT);
-    exit()
+    exit();
   }
 }
 
