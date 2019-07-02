10dd04005778d642c2820dff968a34c208fdf453
kentiss34 <hessquentin@gmail.com>
2019-07-02 17:44:17 +0200

    Fix evol mysql (#108)
    * change init 0.0.4

--- mysql/init/init-0.0.11.sql
+++ mysql/init/init-0.0.11.sql
@@ -1,10 +1,3 @@
-INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_urlbase','');
-INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_http_proto','');
-INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_name','Vigilo');
-INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_language','fr-FR');
-INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_mapquest_api','');
-INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('twitter_expiry_time','24');
-INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('mysql_charset','utf8');
-INSERT INTO obs_config (`config_param`,`config_value`) VALUES ('vigilo_timezone','Europe/Paris');
+
 UPDATE `obs_config` SET `config_value` = '0.0.11' WHERE `obs_config`.`config_param` = 'vigilo_db_version';
 
