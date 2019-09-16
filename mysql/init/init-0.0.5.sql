e7d335941d277b4bb0057924c60bfb21d91aa671
kentiss34 <webmaster@my-heb.eu>
2019-09-16 10:45:48 +0200

    fix install / clear mysql scripts

--- mysql/init/init-0.0.5.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `obs_list` CHANGE `obs_app_version` `obs_app_version` VARCHAR(50) NOT NULL;
-ALTER TABLE `obs_config` ADD PRIMARY KEY( `config_id`);
-UPDATE `obs_config` SET `config_value` = '0.0.5' WHERE `obs_config`.`config_param` = 'vigilo_db_version';
--- mysql/init/init-0.0.6.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `obs_config` SET `config_value` = '0.0.6' WHERE `obs_config`.`config_param` = 'vigilo_db_version';
