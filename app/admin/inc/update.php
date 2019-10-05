5630ff321c83612f1552b0cd705d740802179792
kentiss34 <hessquentin@gmail.com>
2019-10-05 16:22:14 +0200

    migration from admin panel (#154)
    * remove token as a mandatory param
    
    * fix typo
    
    * add logs on admin
    
    * add futur sql migration script
    
    * add comment lenght limitation
    
    * fix Makefile
    
    * add approbation consideration in generate_panel
    
    * fix cache creation if admin
    
    * add gp
    
    * fix pixelisation
    
    * add trick to upload time in seconds instead of ms
    
    * add status change fields in mysql (#67)
    
    * hot fix : create_issue time strlen error
    
    * fix getissues
    
    * add categories filtering list
    
    * add option approved in get_issues
    
    * update doc
    
    * fix mosaic
    
    * add image file check
    
    * fix add_image
    
    * add admin panel
    
    * update admin
    
    * update admin panel
    
    * init 0.0.11
    
    * add requires fileds
    
    * init 0.0.11
    
    * change init 0.0.4
    
    * add new version
    
    * fix typo
    
    * fix non existing postcode
    
    * ifix stdin upload imagex
    
    * fix install / clear mysql scripts
    
    * fix typo
    
    * imove config file + make key in admin optional
    
    * add init sql
    
    * fix transfert encoding in add image
    
    * invert image header checks
    
    * rollback
    
    * fix add image
    
    * missing tabresolved
    
    * fix some bugs
    
    * improve citystaff actions
    
    * add research
    
    * add token in input
    
    * add images
    
    * add cities management
    
    * add filed to list
    
    * add cityname to getissues
    
    * update test script
    
    * ajout ville et caterogie dans message twwets
    
    * ajout doc dans admin
    
    * add city detection in create issue
    
    * fix typo
    
    * missing token in url
    
    * add city to adresse in panel
    
    * add role moderator
    
    * add cache clean when delete an obs from admin
    
    * add field to get_issues for retro compatibility with address & city
    
    * don't update app vertsion when editing obs
    
    * migration via l'admin
    
    * update updates in admin
    
    * add city check

--- app/admin/inc/update.php
+++ app/admin/inc/update.php
@@ -62,3 +62,12 @@ if ($code_version != $last_version) {
     </tbody>
   </table>
 </div>
+<?php
+
+
+// TODO chargé un script sql et le lancer à partir d'ici / si fichier ci dessous existe c'est pour migration/post operation
+if (file_exists('./inc/updates/'.$code_version.'.php')) {
+  echo "<h2>Migration necessaire</h2>";
+  require_once('./inc/updates/'.$codd_version.'.php');
+}
+
