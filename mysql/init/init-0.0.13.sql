a87f2f1f7118925bdc4189f87305c2c62812a21a
root <root@Quentin-debian>
2019-10-05 17:18:07 +0200

    change migration process

--- mysql/init/init-0.0.13.sql
+++ mysql/init/init-0.0.13.sql
@@ -5,3 +5,4 @@ ALTER TABLE `obs_list` ADD `obs_cityname` VARCHAR(255) NOT NULL AFTER `obs_city`
 ALTER TABLE `obs_roles` ADD `role_city` VARCHAR(255) NOT NULL AFTER `role_password`;
 
 UPDATE `obs_config` SET `config_value` = '0.0.13' WHERE `obs_config`.`config_param` = 'vigilo_db_version';
+INSERT INTO obs_config (config_param,config_value) VALUES ("migration_from_version",'0.0.12');
