2c7c84d611ac689f6e7673c81f668c3fe6ab8a7c
kentiss34 <webmaster@my-heb.eu>
2020-07-10 11:58:01 +0200

    create generateimage function

--- app/generate_panel.php
+++ app/generate_panel.php
@@ -16,7 +16,12 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+ini_set('memory_limit','256M');
 
+header('BACKEND_VERSION: '.BACKEND_VERSION);
+header("Content-type: image/jpeg");
+
+$error_prefix = 'GENERATE_PANEL';
 $cwd = dirname(__FILE__);
 
 require_once("${cwd}/includes/common.php");
@@ -35,4 +40,133 @@ if(file_exists('panels/'.$panel_path.'/panel.php')) {
 } else {
   die('Panel not exists');
 }
+
+$caches_path = "${cwd}/caches/";
+$images_path = "${cwd}/images/";
+$maps_path = "${cwd}/maps/";
+$MAX_IMG_SIZE = 1024; // For limit attack
+$resize_width = $MAX_IMG_SIZE; // default width
+$PERCENT_PIXELATED = 10;
+
+if (isset($_GET['key'])) {
+  $key = $_GET['key'];
+}
+else {
+  $key = Null;
+}
+
+/* Token is mandatory */
+if (!isset($_GET['token'])) {
+  jsonError($error_prefix, "Token : ".$token." not provided.", "TOKENNOTPROVIDED", 400);
+}
+
+$token = mysqli_real_escape_string($db, $_GET['token']);
+
+if (isset($_GET['secretid'])) {
+  $secretid = mysqli_real_escape_string($db, $_GET['secretid']);
+}
+else {
+  $secretid = Null;
+}
+
+if (isset($_GET["s"]) and is_numeric($_GET["s"]) and intval($_GET["s"]) <= $MAX_IMG_SIZE) {
+  $resize_width = intval($_GET["s"]);
+  $img_filename = $caches_path . $token . '_w' . $resize_width . '.jpg';
+} else {
+  $img_filename = $caches_path . $token . '_full.jpg';
+}
+
+## Use caches if available
+if (file_exists($img_filename) AND !getrole($key, $acls) == "admin" AND !getrole($key, $acls) == "moderator") {
+  $image = imagecreatefromjpeg($img_filename);
+  imagejpeg($image);
+  return;
+}
+
+# Get issue information
+$query = mysqli_query($db, "SELECT * FROM obs_list WHERE obs_token = '$token' LIMIT 1");
+
+if (mysqli_num_rows($query) != 1) {
+  jsonError($error_prefix, "Token : ".$token." not found.", "TOKENNOTFOUND", 404);
+}
+
+$result = mysqli_fetch_array($query);
+$coordinates_lat = $result['obs_coordinates_lat'];
+$coordinates_lon = $result['obs_coordinates_lon'];
+$street_name = $result['obs_address_string'];
+$comment = $result['obs_comment'];
+$categorie_id = $result['obs_categorie'];
+$statusobs = $obs_status = getObsStatus($db,$result['obs_id']);
+$categorie_string = getCategorieName($categorie_id);
+
+if (!empty($result['obs_city']) && $result['obs_city'] != 0) {
+  $cityquery = mysqli_query($db,"SELECT city_name FROM obs_cities WHERE city_id='".$result['obs_city']."' LIMIT 1");
+  $cityresult = mysqli_fetch_array($cityquery);
+  $street_name .= ', '. $cityresult['city_name'];
+}
+elseif (!empty($result['obs_cityname'])) {
+  $street_name .= ', '. $result['obs_cityname'];
+}
+
+if (!isset($categorie_string)) {
+    jsonError($error_prefix, "unknown categorie_id: ' . $categorie_id", "UNKNOWCATEGORIE", 500);
+}
+
+$time = $result['obs_time'];
+$date = date('d/m/Y H:i', $time);
+
+$approved = $result['obs_approved'];
+if ($secretid == $result['obs_secretid'] OR getrole($key, $acls) == "admin" OR getrole($key, $acls) == "moderator") {
+  $AdminOrAuthor = True;
+}
+else {
+  $AdminOrAuthor = False;
+}
+
+$photo = imagecreatefromjpeg($images_path . $token . '.jpg'); // issue photo
+# Image is pixelated until approved by a moderator
+if ($approved != 1 and !$AdminOrAuthor and $resize_width > 300) {
+
+$tmpImage = ImageCreateTrueColor($photo_max_w, $photo_max_h);
+$pixelated = ImageCreateTrueColor($photo_w, $photo_h);
+
+# First resize the picture - $PERCENT_PIXELATED ratio will increase/decrease data loss
+imagecopyresized($tmpImage, $photo, 0, 0, 0, 0, round($photo_max_w / $PERCENT_PIXELATED), round($photo_max_h / $PERCENT_PIXELATED), $photo_w, $photo_h);
+imagecopyresized($pixelated, $tmpImage, 0, 0, 0, 0, $photo_w, $photo_h, round($photo_max_w / $PERCENT_PIXELATED), round($photo_max_h / $PERCENT_PIXELATED));
+
+# Then apply pixelating + gaussian filters
+imagefilter($pixelated, IMG_FILTER_PIXELATE, 5);
+
+# Gaussian blur reduces pixel effect on small images
+imagefilter($pixelated, IMG_FILTER_GAUSSIAN_BLUR);
+
+$photo = $pixelated;
+}
+$map_file_path=$maps_path . $token . '_zoom.jpg';
+GenerateMapQuestForToken($db,$token,$config['MAPQUEST_API']);
+$map = imagecreatefromjpeg($map_file_path);
+
+$image = GeneratePanel($photo,$map,$comment,$street_name,$token,$categorie_string,$date,$statusobs);
+$background_w = imagesx($image);
+$background_h = imagesy($image);
+
+# Generate full size image
+if ($AdminOrAuthor && $resize_width == $MAX_IMG_SIZE) {
+  imagejpeg($image);
+}
+else if ($resize_width == $MAX_IMG_SIZE) {
+  # Use user original image
+  imagejpeg($image, $img_filename);
+  imagejpeg($image);
+} else {
+  # Resize image
+  $panel_ratio = $background_w / $background_h;
+  $resize_height  = $resize_width / $panel_ratio;
+  $imageresized = imagecreatetruecolor($resize_width, $resize_height);
+
+  imagecopyresampled($imageresized, $image, 0, 0, 0, 0, $resize_width, $resize_height, $background_w, $background_h);
+  imagejpeg($imageresized, $img_filename);
+  imagejpeg($imageresized);
+}
+
 
--- app/panels/jesuisundesdeux/panel.php
+++ app/panels/jesuisundesdeux/panel.php
@@ -16,329 +16,196 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-ini_set('memory_limit','256M');
 
-$cwd = dirname(__FILE__);
-$rootapp = "${cwd}/../..";
+function GeneratePanel($photo,$map,$comment,$street_name,$token,$categorie_string,$date,$statusobs) {
+  # Responsive Photo process
+  $cwd = dirname(__FILE__);
 
-$caches_path = "${rootapp}/caches/";
-$images_path = "${rootapp}/images/";
-$maps_path = "${rootapp}/maps/";
+  $photo_w = imagesx($photo);
+  $photo_h = imagesy($photo);
+  $photo_ratio = $photo_w / $photo_h;
 
-header('BACKEND_VERSION: '.BACKEND_VERSION);
-header("Content-type: image/jpeg");
+  # Portrait format
+  if ($photo_ratio < 1) {
+    $backgroung_image = "${cwd}/panel_components/portrait/background.jpg";
+    $content_image = "${cwd}/panel_components/portrait/content.png";
+    $logo_image = "${cwd}/panel_components/portrait/logo.png";
+    $resolved_image = "${cwd}/panel_components/portrait/resolved.png";
 
-$error_prefix = 'GENERATE_PANEL';
+    # Load background image
+    $image = imagecreatefromjpeg($backgroung_image);
+    $background_w = imagesx($image);
+    $background_h = imagesy($image);
 
-$font_regular = "${cwd}/fonts/texgyreheros-regular.otf";
-$font_italic = "${cwd}/fonts/texgyreheros-italic.otf";
-$font_bold = "${cwd}/fonts/texgyreheros-bold.otf";
+    $photo_max_w = 470;
+    $photo_max_h = 800;
 
-$MAX_IMG_SIZE = 1024; // For limit attack
-$resize_width = $MAX_IMG_SIZE; // default width
-$PERCENT_PIXELATED = 10;
+    $photo_new_size_h = $photo_max_h;
+    $photo_new_size_w = $photo_new_size_h * $photo_ratio ;
 
-if (isset($_GET['key'])) {
-  $key = $_GET['key'];
-}
-else {
-  $key = Null;
-}
-
-/* Token is mandatory */
-if (!isset($_GET['token'])) {
-  jsonError($error_prefix, "Token : ".$token." not provided.", "TOKENNOTPROVIDED", 400);
-}
-
-$token = mysqli_real_escape_string($db, $_GET['token']);
-
-if (isset($_GET['secretid'])) {
-  $secretid = mysqli_real_escape_string($db, $_GET['secretid']);
-}
-else {
-  $secretid = Null;
-}
-
-if (isset($_GET["s"]) and is_numeric($_GET["s"]) and intval($_GET["s"]) <= $MAX_IMG_SIZE) {
-  $resize_width = intval($_GET["s"]);
-  $img_filename = $caches_path . $token . '_w' . $resize_width . '.jpg';
-} else {
-  $img_filename = $caches_path . $token . '_full.jpg';
-}
-
-## Use caches if available
-if (file_exists($img_filename) AND !getrole($key, $acls) == "admin" AND !getrole($key, $acls) == "moderator") {
-  $image = imagecreatefromjpeg($img_filename);
-  imagejpeg($image);
-  return;
-}
-
-# Get issue information
-$query = mysqli_query($db, "SELECT * FROM obs_list WHERE obs_token = '$token' LIMIT 1");
-
-if (mysqli_num_rows($query) != 1) {
-  jsonError($error_prefix, "Token : ".$token." not found.", "TOKENNOTFOUND", 404);
-}
-
-$result = mysqli_fetch_array($query);
-$coordinates_lat = $result['obs_coordinates_lat'];
-$coordinates_lon = $result['obs_coordinates_lon'];
-$street_name = $result['obs_address_string'];
-$comment = $result['obs_comment'];
-$categorie_id = $result['obs_categorie'];
-$statusobs = $obs_status = getObsStatus($db,$result['obs_id']);
-
-$categorie_string = getCategorieName($categorie_id);
-
-
-if (!empty($result['obs_city']) && $result['obs_city'] != 0) {
-  $cityquery = mysqli_query($db,"SELECT city_name FROM obs_cities WHERE city_id='".$result['obs_city']."' LIMIT 1");
-  $cityresult = mysqli_fetch_array($cityquery);
-  $street_name .= ', '. $cityresult['city_name'];
-}
-elseif (!empty($result['obs_cityname'])) {
-  $street_name .= ', '. $result['obs_cityname'];
-}
-
-
-if (!isset($categorie_string)) {
-    jsonError($error_prefix, "unknown categorie_id: ' . $categorie_id", "UNKNOWCATEGORIE", 500);
-}
-
-$time = $result['obs_time'];
-$approved = $result['obs_approved'];
-if ($secretid == $result['obs_secretid'] OR getrole($key, $acls) == "admin" OR getrole($key, $acls) == "moderator") {
-  $AdminOrAuthor = True;
-}
-else {
-  $AdminOrAuthor = False;
-}
-
-##################################### COMPOSING IMAGE #########################"
-## Init other images components :
-$photo = imagecreatefromjpeg($images_path . $token . '.jpg'); // issue photo
-$photo_w = imagesx($photo);
-$photo_h = imagesy($photo);
-$photo_ratio = $photo_w / $photo_h;
-
-# Portrait format
-if ($photo_ratio < 1) {
-  $backgroung_image = "${cwd}/panel_components/portrait/background.jpg";
-  $content_image = "${cwd}/panel_components/portrait/content.png";
-  $logo_image = "${cwd}/panel_components/portrait/logo.png";
-  $resolved_image = "${cwd}/panel_components/portrait/resolved.png";
-
-  # Load background image
-  $image = imagecreatefromjpeg($backgroung_image);
-  $background_w = imagesx($image);
-  $background_h = imagesy($image);
-
-  $photo_max_w = 470;
-  $photo_max_h = 800;
-
-  $photo_new_size_h = $photo_max_h;
-  $photo_new_size_w = $photo_new_size_h * $photo_ratio ;
-
-  while ($photo_new_size_w > $photo_max_w or $photo_new_size_h > $photo_max_h) {
-    $photo_new_size_w--;
-    $photo_new_size_h = $photo_new_size_w / $photo_ratio;
-  }
-
-  $photo_position_x = 380;
-  $photo_min_y = $background_h  - (103 + $photo_new_size_h);
-  $map_x = -10;
-  $map_y = 543;
-  $comment_y_frombottom = 50;
-  $comment_span_x = 0;
-  $categorie_y = 240;
-  $address_y = 330;
-  $date_y = 515;
-  $copyright_x= 2;
+    while ($photo_new_size_w > $photo_max_w or $photo_new_size_h > $photo_max_h) {
+      $photo_new_size_w--;
+      $photo_new_size_h = $photo_new_size_w / $photo_ratio;
+    }
+ 
+    $photo_position_x = 380;
+    $photo_min_y = $background_h  - (103 + $photo_new_size_h);
+    $map_x = -10;
+    $map_y = 543;
+    $comment_y_frombottom = 50;
+    $comment_span_x = 0;
+    $categorie_y = 240;
+    $address_y = 330;
+    $date_y = 515;
+    $copyright_x= 2;
   
-  $resolved_x = 520;
-  $resolved_y = -15;
-  $resolved_w = 300;
-  $resolved_h = 150;
+    $resolved_x = 520;
+    $resolved_y = -15;
+    $resolved_w = 300;
+    $resolved_h = 150;
 
-}
-# Landscape format
-else {
-  $backgroung_image = "${cwd}/panel_components/landscape/background.jpg";
-  $content_image = "${cwd}/panel_components/landscape/content.png";
-  $logo_image = "${cwd}/panel_components/landscape/logo.png";
-  $resolved_image = "${cwd}/panel_components/landscape/resolved.png";
+  }
+  # Landscape format
+  else {
+    $backgroung_image = "${cwd}/panel_components/landscape/background.jpg";
+    $content_image = "${cwd}/panel_components/landscape/content.png";
+    $logo_image = "${cwd}/panel_components/landscape/logo.png";
+    $resolved_image = "${cwd}/panel_components/landscape/resolved.png";
+  
+    # Load background image
+    $image = imagecreatefromjpeg($backgroung_image);
+    $background_w = imagesx($image);
+    $background_h = imagesy($image);
 
-  # Load background image
-  $image = imagecreatefromjpeg($backgroung_image);
-  $background_w = imagesx($image);
-  $background_h = imagesy($image);
+    $photo_max_w = 900;
+    $photo_max_h = 600;
 
-  $photo_max_w = 900;
-  $photo_max_h = 600;
-
-  $photo_new_size_h = $photo_max_h;
-  $photo_new_size_w = $photo_new_size_h * $photo_ratio ;
-  while ($photo_new_size_w > $photo_max_w or $photo_new_size_h > $photo_max_h) {
-    $photo_new_size_h--;
-    $photo_new_size_w = $photo_new_size_h * $photo_ratio;
+    $photo_new_size_h = $photo_max_h;
+    $photo_new_size_w = $photo_new_size_h * $photo_ratio ;
+    while ($photo_new_size_w > $photo_max_w or $photo_new_size_h > $photo_max_h) {
+      $photo_new_size_h--;
+      $photo_new_size_w = $photo_new_size_h * $photo_ratio;
+    }
+  
+    $photo_position_x = $background_w-$photo_new_size_w;
+    $photo_min_y = 103;
+    $map_x = -5;
+    $map_y = 438;
+    $comment_y_frombottom = 40;
+    $comment_span_x = 423;
+    $categorie_y = 250;
+    $address_y = 330;
+    $date_y = 415;
+    $copyright_x= 280;
+ 
+    $resolved_x = 960;
+    $resolved_y = -15;
+    $resolved_w = 300;
+    $resolved_h = 150;
   }
 
-  $photo_position_x = $background_w-$photo_new_size_w;
-  $photo_min_y = 103;
-  $map_x = -5;
-  $map_y = 438;
-  $comment_y_frombottom = 40;
-  $comment_span_x = 423;
-  $categorie_y = 250;
-  $address_y = 330;
-  $date_y = 415;
-  $copyright_x= 280;
+  ## INIT IMAGE ##
+  $font_regular = "${cwd}/fonts/texgyreheros-regular.otf";
+  $font_italic = "${cwd}/fonts/texgyreheros-italic.otf";
+  $font_bold = "${cwd}/fonts/texgyreheros-bold.otf";
 
-  $resolved_x = 960;
-  $resolved_y = -15;
-  $resolved_w = 300;
-  $resolved_h = 150;
-}
+  $fontcolor = imagecolorallocate($image, 54, 66, 86);
+  $fontcolorgrey = imagecolorallocate($image, 219, 219,219);
+  $white = imagecolorallocate($image, 255, 255, 255);
+  $black = imagecolorallocate($image, 0, 0, 0);
+  $green = imagecolorallocate($image,35, 122, 68);
 
-## INIT IMAGE ##
-$fontcolor = imagecolorallocate($image, 54, 66, 86);
-$fontcolorgrey = imagecolorallocate($image, 219, 219,219);
-$white = imagecolorallocate($image, 255, 255, 255);
-$black = imagecolorallocate($image, 0, 0, 0);
-$green = imagecolorallocate($image,35, 122, 68);
+  ## ADD PHOTO
+  ## Photo
+  $photo_position_y = $photo_min_y;
 
-## ADD PHOTO
-## Photo
-# $photo_position_x = $background_w-$photo_new_size_w;
-$photo_position_y = $photo_min_y;
+  imagecopyresized($image, $photo, $photo_position_x, $photo_position_y, 0, 0, $photo_new_size_w, $photo_new_size_h, $photo_w, $photo_h);
 
-# Image is pixelated until approved by a moderator
-if ($approved != 1 and !$AdminOrAuthor and $resize_width > 300) {
+  $mask = imagecreatetruecolor(360,360);
+  $transparent = imagecolorallocate($mask, 255, 0, 0);
+  $alpha = imagecolorallocate($mask, 0, 0, 0);
 
-  $tmpImage = ImageCreateTrueColor($photo_max_w, $photo_max_h);
-  $pixelated = ImageCreateTrueColor($photo_w, $photo_h);
+  $map_circle = imagecreatetruecolor(360, 360);
+  imagecopymerge($map_circle,$map,0,0,0,0,imagesx($map),imagesy($map),100);
 
-  # First resize the picture - $PERCENT_PIXELATED ratio will increase/decrease data loss
-  imagecopyresized($tmpImage, $photo, 0, 0, 0, 0, round($photo_max_w / $PERCENT_PIXELATED), round($photo_max_h / $PERCENT_PIXELATED), $photo_w, $photo_h);
-  imagecopyresized($pixelated, $tmpImage, 0, 0, 0, 0, $photo_w, $photo_h, round($photo_max_w / $PERCENT_PIXELATED), round($photo_max_h / $PERCENT_PIXELATED));
+  imagealphablending($map_circle, true);
 
-  # Then apply pixelating + gaussian filters
-  imagefilter($pixelated, IMG_FILTER_PIXELATE, 5);
+  imagecolortransparent($mask,$transparent);
 
-  # Gaussian blur reduces pixel effect on small images
-  imagefilter($pixelated, IMG_FILTER_GAUSSIAN_BLUR);
+  imagefilledellipse($mask, 360/2, 360/2, 360, 360, $transparent);
+  imagecopymerge($map_circle, $mask, 0, 0, 0, 0, 360, 360, 100);
+  imagecolortransparent($map_circle,$alpha);
+  imagefill($map_circle, 0, 0, $alpha);
 
-  $photo = $pixelated;
-}
+  imagecopymerge($image, $map_circle, $map_x, $map_y, 0, 0, 360, 360, 100);
 
-imagecopyresized($image, $photo, $photo_position_x, $photo_position_y, 0, 0, $photo_new_size_w, $photo_new_size_h, $photo_w, $photo_h);
+  ## ADD MAP COPYRIGHTS ##
+  imagettftext($image, 7, 0, $copyright_x, $background_h-13, $white, $font_regular, "©2019 MAPQUEST ©OPENSTREETMAP ©MAPBOX");
 
-## ADD MAP ##
-$map_download_path_zoom=$maps_path . $token . '_zoom.jpg';
-GenerateMapQuestForToken($db,$token,$config['MAPQUEST_API']);
+  ## ADD CONTENT BLOCK ##
+  $content_block = imagecreatefrompng($content_image);
+  imagecopy($image, $content_block, 0, 0, 0, 0, imagesx($content_block), imagesy($content_block));
 
-$map_zoom = imagecreatefromjpeg($map_download_path_zoom);
-$mask = imagecreatetruecolor(360,360);
-$transparent = imagecolorallocate($mask, 255, 0, 0);
-$alpha = imagecolorallocate($mask, 0, 0, 0);
+  ## ADD LOGO ##
+  $logo = imagecreatefrompng($logo_image); // logo #JeSuisUnDesDeux
+  imagecopy($image, $logo,0,0,0,0,imagesx($logo),imagesy($logo));
 
-$map_zoom_circle = imagecreatetruecolor(360, 360);
-imagecopymerge($map_zoom_circle,$map_zoom,0,0,0,0,imagesx($map_zoom),imagesy($map_zoom),100);
+  ## ADD COMMENT
+  $comment_color = imagecolorallocate($image, 255,219,80);
+  $comment_font_size = 25;
 
-imagealphablending($map_zoom_circle, true);
+  if (!empty($comment)) {
+    $comment = '"'.trim($comment) . '"';
+    $comment_box = imagettfbbox($comment_font_size, 0, $font_italic, $comment);
+    $comment_x = (($background_w - $comment_span_x) - ($comment_box[2] - $comment_box[0])) / 2;
+    imagettftext($image,$comment_font_size ,0,$comment_span_x + $comment_x,$background_h-$comment_y_frombottom,$comment_color,$font_italic,$comment);
+  }
 
-imagecolortransparent($mask,$transparent);
+  ## ADD TOKEN
+  $token_font_size = 25;
+  imagettftext($image,$token_font_size,0,200,160,$black,$font_regular,$token);
 
-imagefilledellipse($mask, 360/2, 360/2, 360, 360, $transparent);
-imagecopymerge($map_zoom_circle, $mask, 0, 0, 0, 0, 360, 360, 100);
-imagecolortransparent($map_zoom_circle,$alpha);
-imagefill($map_zoom_circle, 0, 0, $alpha);
+  ## ADD CATEGORIE
+  $categorie_font_size = 26;
+  $categorie_max_char_per_line = 18;
 
-imagecopymerge($image, $map_zoom_circle, $map_x, $map_y, 0, 0, 360, 360, 100);
-
-## ADD MAP COPYRIGHTS ##
-imagettftext($image, 7, 0, $copyright_x, $background_h-13, $white, $font_regular, "©2019 MAPQUEST ©OPENSTREETMAP ©MAPBOX");
-
-## ADD CONTENT BLOCK ##
-$content_block = imagecreatefrompng($content_image);
-imagecopy($image, $content_block, 0, 0, 0, 0, imagesx($content_block), imagesy($content_block));
-
-## ADD LOGO ##
-$logo = imagecreatefrompng($logo_image); // logo #JeSuisUnDesDeux
-imagecopy($image, $logo,0,0,0,0,imagesx($logo),imagesy($logo));
-
-## ADD COMMENT
-$comment_color = imagecolorallocate($image, 255,219,80);
-$comment_font_size = 25;
-
-if (!empty($comment)) {
-  $comment = '"'.trim($comment) . '"';
-  $comment_box = imagettfbbox($comment_font_size, 0, $font_italic, $comment);
-  $comment_x = (($background_w - $comment_span_x) - ($comment_box[2] - $comment_box[0])) / 2;
-  imagettftext($image,$comment_font_size ,0,$comment_span_x + $comment_x,$background_h-$comment_y_frombottom,$comment_color,$font_italic,$comment);
-}
-
-## ADD TOKEN
-$token_font_size = 25;
-imagettftext($image,$token_font_size,0,200,160,$black,$font_regular,$token);
-
-## ADD CATEGORIE
-$categorie_font_size = 26;
-$categorie_max_char_per_line = 18;
-
-$categorie_string_formatted = wordwrap($categorie_string, $categorie_max_char_per_line, "===");
-$categories_nblines = substr_count($categorie_string_formatted,"===");
-
-if($categories_nblines > 1) {
-  $categorie_max_char_per_line = 25;
   $categorie_string_formatted = wordwrap($categorie_string, $categorie_max_char_per_line, "===");
+  $categories_nblines = substr_count($categorie_string_formatted,"===");
+
+  if($categories_nblines > 1) {
+    $categorie_max_char_per_line = 25;
+    $categorie_string_formatted = wordwrap($categorie_string, $categorie_max_char_per_line, "===");
+  }
+
+  $categories_lines = explode('===',$categorie_string_formatted);
+  foreach($categories_lines as $categories_line) {
+    imagettftext($image,$categorie_font_size,0,29,$categorie_y,$black,$font_bold,$categories_line);
+    $categorie_y += 35;
+  } 
+
+  ## ADD ADDRESS
+  $address_font_size = 18;
+  $address_max_char_per_line = 25;
+  $street_name = wordwrap($street_name,$address_max_char_per_line,"===");
+  
+  $address_lines = explode('===',$street_name);
+  
+  foreach($address_lines as $address_line) {
+    imagettftext($image,$address_font_size,0,29,$address_y,$black,$font_bold,$address_line);
+    $address_y += 28;
+  }
+
+  ## ADD DATE
+  $date_font_size = 15;
+  imagettftext($image,$date_font_size,0,29,$date_y,$black,$font_regular,$date);
+  
+  ## ADD RESOLVED
+  if($statusobs == 1) {
+    $resolved = imagecreatefrompng($resolved_image);
+    imagecopyresized($image, $resolved, $resolved_x, $resolved_y, 0, 0, $resolved_w, $resolved_h,imagesx($resolved), imagesy($resolved));
+  }
+
+  return $image;
 }
 
-$categories_lines = explode('===',$categorie_string_formatted);
-foreach($categories_lines as $categories_line) {
-  imagettftext($image,$categorie_font_size,0,29,$categorie_y,$black,$font_bold,$categories_line);
-  $categorie_y += 35;
-}
-
-## ADD ADDRESS
-$address_font_size = 18;
-$address_max_char_per_line = 25;
-$street_name = wordwrap($street_name,$address_max_char_per_line,"===");
-
-$address_lines = explode('===',$street_name);
-
-foreach($address_lines as $address_line) {
-  imagettftext($image,$address_font_size,0,29,$address_y,$black,$font_bold,$address_line);
-  $address_y += 28;
-}
-
-## ADD DATE
-$date = date('d/m/Y H:i', $time);
-$date_font_size = 15;
-imagettftext($image,$date_font_size,0,29,$date_y,$black,$font_regular,$date);
-
-## ADD RESOLVED
-if($statusobs == 1) {
-  $resolved = imagecreatefrompng($resolved_image);
-  imagecopyresized($image, $resolved, $resolved_x, $resolved_y, 0, 0, $resolved_w, $resolved_h,imagesx($resolved), imagesy($resolved));
-}
-
-# Generate full size image
-if ($AdminOrAuthor && $resize_width == $MAX_IMG_SIZE) {
-  imagejpeg($image);
-}
-else if ($resize_width == $MAX_IMG_SIZE) {
-  # Use user original image
-  imagejpeg($image, $img_filename);
-  imagejpeg($image);
-} else {
-  # Resize image
-  $panel_ratio = $background_w / $background_h;
-  $resize_height  = $resize_width / $panel_ratio;
-  $imageresized = imagecreatetruecolor($resize_width, $resize_height);
-
-  imagecopyresampled($imageresized, $image, 0, 0, 0, 0, $resize_width, $resize_height, $background_w, $background_h);
-  imagejpeg($imageresized, $img_filename);
-  imagejpeg($imageresized);
-}
 
