795be5595ab0e706b3a2f7ef0e2d9cff7fceec1e
tounoki <nico.public@ouvaton.org>
2020-10-30 16:21:26 +0100

    Création d'un fichier externe js

--- app/admin/inc/cities.php
+++ app/admin/inc/cities.php
@@ -96,11 +96,14 @@ while ($result_scopes = mysqli_fetch_array($query_scopes)) {
 }
 
 while ($result_cities = mysqli_fetch_array($query_cities)) {
+    $idForm = "city".$result_cities['city_id']."form" ;
 ?>
 
-      <form action="" method="POST" name="<?= $result_cities['city_name']."form" ?>" >
+      <form action="" method="POST" id="<?= $idForm ?>" name="<?= $idForm ?>" >
       <tr>
-        <td>#<?= $result_cities['city_id'] ?></td>
+        <td>
+          #<?= $result_cities['city_id'] ?>
+        </td>
         <td>
           <input type="text" class="form-control-plaintext" name="city_name" value="<?= $result_cities['city_name'] ?>" required />
         </td>
@@ -139,7 +142,7 @@ while ($result_cities = mysqli_fetch_array($query_cities)) {
         <td>
           <a href="?page=<?= $page_name ?>&action=delete&cityid=<?= $result_cities['city_id'] ?>" onclick="return confirm('Merci de valider la suppression')"><span data-feather="delete"></span> Supprimer</a>
           <br/>
-          <a href="#" onclick="getWikidata( document.forms['<?= $result_cities['city_name']."form" ; ?>'] )" ><span data-feather="help-circle"></span> Wikidata</a>
+          <a href="#<?= $idForm ?>" onclick="getWikidata( document.forms['<?= $idForm ?>'] );" ><span data-feather="help-circle"></span> Wikidata</a>
         </td>
       </tr>
       </form>
@@ -151,78 +154,4 @@ while ($result_cities = mysqli_fetch_array($query_cities)) {
 </div>
 <br />
 
-<script>
-class SPARQLQueryDispatcher {
-	constructor( endpoint ) {
-		this.endpoint = endpoint;
-	}
-
-	query( sparqlQuery ) {
-		const fullUrl = this.endpoint + '?query=' + encodeURIComponent( sparqlQuery );
-		const headers = { 'Accept': 'application/sparql-results+json' };
-
-		return fetch( fullUrl, { headers } ).then( body => body.json() );
-	}
-}
-function getWikidata( frm ) {
-    var city_name = frm.elements['city_name'].value ;
-    const endpointUrl = 'https://query.wikidata.org/sparql';
-    const sparqlQuery = `SELECT DISTINCT ?ville ?name ?villeLabel ?CP ?Population ?Area ?Sitewww WHERE {
-          VALUES ?name { "` + city_name + `"@fr }
-          ?ville wdt:P31 wd:Q484170;
-            rdfs:label ?name;
-            OPTIONAL {?ville wdt:P281 ?CP.}
-            OPTIONAL {?ville wdt:P856 ?Sitewww. }
-            OPTIONAL {?ville wdt:P1082 ?Population. }
-            OPTIONAL {?ville wdt:P2046 ?Area.}
-          SERVICE wikibase:label { bd:serviceParam wikibase:language "fr". }
-        }
-        LIMIT 10
-        `;
-    const queryDispatcher = new SPARQLQueryDispatcher( endpointUrl );
-    queryDispatcher.query( sparqlQuery ).then( console.log );
-    queryDispatcher.query( sparqlQuery )
-              .then( (ret) => {
-                var nb = ret.results.bindings.length ;
-                if ( nb == 0 ) {
-                  alert('Pas de résultats :( ') ;
-                  return true ;
-                }
-
-                if ( nb > 0 ){
-                    var bien = ( nb == 1 ) ? " bien " : " " ;
-                    var txt, ordre ;
-                    // on fait une boucle pour proposer chaque résultat
-                    for ( var k = 0 ; k < nb ; k++ ) {
-                        txt = "" ;
-                        ordre = k+1 ;
-                        txt += "Est-ce"+bien+"(proposition "+ ordre +"/"+nb+") :" ;
-                        if(typeof ret.results.bindings[k].name != 'undefined')
-                            txt += "\nNom de la ville: " + ret.results.bindings[0].name.value ;
-                        if(typeof ret.results.bindings[k].Area != 'undefined')
-                            txt += "\nAire: " + ret.results.bindings[k].Area.value ;
-                        if(typeof ret.results.bindings[k].Population != 'undefined')
-                            txt += "\nPopulation: " + ret.results.bindings[k].Population.value ;
-                        if(typeof ret.results.bindings[k].CP != 'undefined')
-                            txt += "\nCode Postal: " + ret.results.bindings[k].CP.value ;
-                        if(typeof ret.results.bindings[k].Sitewww != 'undefined')
-                            txt += "\nSite Internet: " + ret.results.bindings[k].Sitewww.value ;
-
-                        if ( confirm(txt) ) {
-                            if(typeof ret.results.bindings[k].name != 'undefined')
-                                frm.elements['city_postcode'].value = ret.results.bindings[k].CP.value ;
-                            if(typeof ret.results.bindings[k].Area != 'undefined')
-                                frm.elements['city_area'].value = ret.results.bindings[k].Area.value ;
-                            if(typeof ret.results.bindings[k].Population != 'undefined')
-                                frm.elements['city_population'].value = ret.results.bindings[k].Population.value ;
-                            if(typeof ret.results.bindings[k].Sitewww != 'undefined')
-                                frm.elements['city_website'].value = ret.results.bindings[k].Sitewww.value ;
-                        }
-                    }
-                }
-
-                return true ;
-              } );
-    return true ;
-}
-</script>
+<script type="text/javascript" src="js/wikidata.js"></script>
